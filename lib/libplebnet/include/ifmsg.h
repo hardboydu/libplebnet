d53bc15ce6f416ea7275ba1c75018a28a6fe0c17
K M Macy <kmacy@freebsd.org>
2011-08-19 18:34:02 +0000

    - mark message structures as packed

--- lib/libplebnet/include/ifmsg.h
+++ lib/libplebnet/include/ifmsg.h
@@ -22,14 +22,14 @@ struct ioctl_call_msg {
 	unsigned long icm_request;
 	char icm_data[0];
 
-};
+}__attribute__((packed));
 
 struct ifreq_call_msg {
 	int icm_fd;
 	unsigned long icm_request;
 	struct ifreq icm_ifr;
 	char icm_ifr_data[0];
-};
+}__attribute__((packed));
 
 struct ifreq_return_msg {
 	struct ifreq irm_ifr;
@@ -41,38 +41,46 @@ struct ifconf_call_msg {
 	int icm_fd;
 	unsigned long icm_request;
 	int icm_ifc_len;
-};
+}__attribute__((packed));
 
 struct ifconf_return_msg {
 	int irm_ifc_len;
 	char irm_ifconf_data[0];
 };
 
-
 struct ifgroup_call_msg {
 	int icm_fd;
 	unsigned long icm_request;
 	struct ifgroupreq icm_ifgrq;
 	char icm_ifgrq_data[0];
-};
+}__attribute__((packed));
 
 struct ifgroup_return_msg {
 	struct ifgroupreq irm_ifgrq;
 	char irm_ifgrq_data[0];
 };
 
-
 struct ifclonereq_call_msg {
 	int icm_fd;
 	unsigned long icm_request;
 	int icm_ifcr_count;
+}__attribute__((packed));
+
+struct ifclonereq_return_msg {
+	int irm_total;
+	char irm_buffer[0];
 };
 
-struct if_clonereq_return_msg {
-	int ifcrm_total;
-	char ifcrm_buffer[0];
-};
+struct ifmediareq_call_msg {
+	int icm_fd;
+	unsigned long icm_request;
+	struct ifmediareq icm_ifmr;
+}__attribute__((packed));
 
+struct ifmediareq_return_msg {
+	struct ifmediareq icm_ifmr;
+	char icm_ifmr_data[0];
+};
 
 /* other system calls */
 
@@ -80,7 +88,7 @@ struct socket_call_msg {
 	int scm_domain; 
 	int scm_type; 
 	int scm_protocol;
-};
+}__attribute__((packed));
 
 struct socket_return_msg {
 	int srm_fd;
